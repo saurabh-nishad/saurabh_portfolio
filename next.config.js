599db4b3ec3fbd161061ba78c84b550862a48fd8
Saurabh Nishad <saurabh.nishad@outlook.com>
2026-03-08 02:17:00 +0530

    Use next.config.js, add memory limit, upload build log on failure

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  output: "export",
+  basePath: process.env.BASE_PATH || "",
+  assetPrefix: process.env.BASE_PATH ? process.env.BASE_PATH + "/" : "",
+  trailingSlash: true,
+  images: { unoptimized: true },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+};
